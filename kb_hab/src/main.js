2979910d3ed24b935033afadb84ec22d25e23c22
bagjuna <juna.park124@gmail.com>
2025-04-07 14:21:13 +0900

    #1- project setting

---
+++ kb_hab/src/main.js
@@ -0,0 +1,28 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+
+import App from './App.vue'
+import router from './router'
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faUser, faEnvelope, faHouse } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUser, faEnvelope, faHouse)
+const app = createApp(App)
+
+app.use(createPinia()).use(ElementPlus)
+
+app.use(router).component('font-awesome-icon', FontAwesomeIcon)
+
+app.mount('#app')
