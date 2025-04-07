6002fa4a0c2211da07f4cf4d665b59fc92387357
bagjuna <juna.park124@gmail.com>
2025-04-07 16:03:29 +0900

    #1 install calendar

--- kb_hab/src/main.js
+++ kb_hab/src/main.js
@@ -5,6 +5,7 @@ import { createPinia } from 'pinia'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 
 import App from './App.vue'
@@ -24,5 +25,9 @@ const app = createApp(App)
 app.use(createPinia()).use(ElementPlus)
 
 app.use(router).component('font-awesome-icon', FontAwesomeIcon)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.mount('#app')
