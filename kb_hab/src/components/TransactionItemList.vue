29442a07765988dd57993267edc68f49d78f4c24
6uohul <kiy010604@naver.com>
2025-04-08 16:53:55 +0900

    feat/#28-TransactionItemList 컴포넌트 구현

---
+++ kb_hab/src/components/TransactionItemList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="display: flex; flex-direction: column; gap: 12px">
+    <TransactionItem v-for="item in transactions" :key="item.id" :transaction="item" />
+  </div>
+</template>
+
+<script setup>
+import TransactionItem from './TransactionItem.vue'
+
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  },
+})
+</script>
