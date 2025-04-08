92741457837c8baa1d39e45bcbb94ceebea3d50c
6uohul <kiy010604@naver.com>
2025-04-08 23:38:21 +0900

    feat/#43-TransactionHistory에 list 컴포넌트 추가

--- kb_hab/src/pages/TransactionHistory.vue
+++ kb_hab/src/pages/TransactionHistory.vue
@@ -1,5 +1,10 @@
 <template>
-  <h2>
-  TransactionHistory
-</h2>
+  <div style="background-color: #ffffff; padding: 20px">
+    <TransactionItemList :transactions="dummyTransactions" />
+  </div>
 </template>
+
+<script setup>
+import TransactionItemList from '@/components/Transaction/TransactionItemList.vue'
+import { dummyTransactions } from '@/data/transactions.js'
+</script>
