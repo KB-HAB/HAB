51b34c7eada4dd5834cbb7c3e626ec549e2cb60e
6uohul <kiy010604@naver.com>
2025-04-08 23:42:02 +0900

    feat/#43-History에서 Detail 클릭이벤트 추가

--- kb_hab/src/pages/TransactionHistory.vue
+++ kb_hab/src/pages/TransactionHistory.vue
@@ -1,10 +1,17 @@
 <template>
   <div style="background-color: #ffffff; padding: 20px">
-    <TransactionItemList :transactions="dummyTransactions" />
+    <TransactionItemList :transactions="dummyTransactions" @click="goToDetail" />
   </div>
 </template>
 
 <script setup>
 import TransactionItemList from '@/components/Transaction/TransactionItemList.vue'
 import { dummyTransactions } from '@/data/transactions.js'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const goToDetail = (id) => {
+  router.push(`/transactions/${id}`)
+}
 </script>
