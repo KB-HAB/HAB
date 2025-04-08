2bc36696ba49b458e07737c9762e70205663d3cb
Gyehyun Park <gyehyunbak@gmail.com>
2025-04-08 16:52:59 +0900

    feat/#26 InputWithLength에서 maxLength 이상 입력 못하게 구현

--- kb_hab/src/components/common/InputWithLength.vue
+++ kb_hab/src/components/common/InputWithLength.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="space-y-1">
-    <CommonInput v-model="text" :placeholder="placeholder" v-bind="$attrs" />
+    <CommonInput v-model="text" :placeholder="placeholder" :maxlength="maxLength" v-bind="$attrs" />
     <div class="text-right text-sm text-gray-400">{{ text.length }} / {{ maxLength }}</div>
   </div>
 </template>
@@ -28,7 +28,7 @@ const emit = defineEmits(['update:modelValue'])
 
 const text = ref(props.modelValue)
 
-// modelValue가 외부에서 변경됐을 때 반영
+// 외부에서 modelValue 변경 시 반영
 watch(
   () => props.modelValue,
   (val) => {
@@ -36,7 +36,7 @@ watch(
   },
 )
 
-// 내부 text 변경 시 부모에게 반영
+// 내부 값 변경 시 emit
 watch(text, (val) => {
   emit('update:modelValue', val)
 })
