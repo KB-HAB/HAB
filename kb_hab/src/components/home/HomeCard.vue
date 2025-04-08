b8ba8bec9f597f8a49c367ff8e7be8091cdfbaf9
doheun9489 <doheun9489@gmail.com>
2025-04-08 16:00:33 +0900

    feat#23 HomeCard 컴포넌트 구현

---
+++ kb_hab/src/components/home/HomeCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- Header  -->
+  <div class="rounded-xl shadow-md p-4 w-full flex-1 h-[98px]" :class="[bgColor, textColor]">
+    <div class="flex flex-col justify-between h-full">
+      <span class="text-left self-start text-sm">{{ title }}</span>
+      <span class="text-lg font-semibold self-end">{{ formattedAmount }} 원</span>
+    </div>
+  </div>
+  <!-- NavBar -->
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: '제목 없음',
+  },
+  amount: {
+    type: Number,
+    default: 0,
+  },
+  bgColor: {
+    type: String,
+    default: 'bg-gray-100',
+  },
+  textColor: {
+    type: String,
+    default: 'text-black',
+  },
+  iconButton: {},
+})
+
+const formattedAmount = computed(() => props.amount.toLocaleString())
+</script>
--- kb_hab/src/pages/HomePage.vue
+++ kb_hab/src/pages/HomePage.vue
@@ -1,3 +1,64 @@
 <template>
-  <h2>HomePage</h2>
+  <!-- <Header /> -->
+  <div class="p-4 space-y-6">
+    <!-- 하루 쓸 수 있는 돈 -->
+    <div>
+      <HomeCard
+        title="이대로 가면 하루에 쓸 수 있는 돈"
+        :amount="weeklyAmount"
+        bg-color="bg-green-500"
+        textColor="text-white"
+      />
+    </div>
+
+    <div class="flex gap-3">
+      <!-- 일주일 -->
+      <HomeCard
+        title="일주일"
+        :amount="weeklyAmount"
+        bgColor="bg-gray-800"
+        textColor="text-white"
+      />
+      <!-- 남은 돈 -->
+      <HomeCard
+        title="남은 돈 "
+        :amount="remainingAmount"
+        bgColor="bg-gray-100"
+        textColor="text-black"
+      />
+    </div>
+  </div>
+  <!-- <NavBar /> -->
 </template>
+
+<script setup>
+import { ref, computed } from 'vue'
+import HomeCard from '@/components/home/HomeCard.vue'
+
+const dailyAmount = ref(5789)
+const weeklyAmount = ref(40523)
+const remainingAmount = ref(247932)
+
+const income = ref(10000)
+const expense = ref(10000)
+const netIncome = computed(() => income.value - expense.value)
+
+const currentDate = ref(new Date())
+const formattedMonth = computed(() => {
+  const year = currentDate.value.getFullYear()
+  const month = String(currentDate.value.getMonth() + 1).padStart(2, '0')
+  return `${year}.${month}월`
+})
+
+const prevMonth = () => {
+  const date = new Date(currentDate.value)
+  date.setMonth(date.getMonth() - 1)
+  currentDate.value = date
+}
+
+const nextMonth = () => {
+  const date = new Date(currentDate.value)
+  date.setMonth(date.getMonth() + 1)
+  currentDate.value = date
+}
+</script>
