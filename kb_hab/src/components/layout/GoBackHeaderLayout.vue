985b9fdd077dc471918b84f10b24acb53e011e49
Gyehyun Park <gyehyunbak@gmail.com>
2025-04-09 16:24:56 +0900

    feat/ GoBackHeaderLayout 구현

---
+++ kb_hab/src/components/layout/GoBackHeaderLayout.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ChevronLeft } from 'lucide-vue-next'
+import IconButton from '../common/IconButton.vue'
+import { useRouter } from 'vue-router'
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+})
+
+const router = useRouter()
+
+const handleClick = () => {
+  router.back()
+}
+</script>
+
+<template>
+  <div class="flex w-full h-[48px] items-center bg-white">
+    <IconButton :onClick="handleClick">
+      <ChevronLeft />
+    </IconButton>
+    <span class="text-lg font-semibold ml-2">{{ props.title }}</span>
+  </div>
+</template>
