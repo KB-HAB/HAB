b0a7ff4546a3bba4d9115dcdf8d106fbf4f36f07
bagjuna <juna.park124@gmail.com>
2025-04-10 19:41:16 +0900

    임시 완성

--- kb_hab/src/components/home/MonthlyCard.vue
+++ kb_hab/src/components/home/MonthlyCard.vue
@@ -42,13 +42,13 @@
       <HomeCard
         title="수입"
         type="income"
-        :amount="income"
+        :amount="props.income"
         bgColor="bg-[#4B4B4B]"
         textColor="text-white"
       />
       <HomeCard
         title="지출"
-        type="expenditure"
+        type="props.expenditure"
         :amount="expenditure"
         bgColor="bg-[#F8F8F8]"
         textColor="text-black"
@@ -73,10 +73,6 @@ import { ChevronLeft, ChevronRight, CalendarDays } from 'lucide-vue-next'
 import HomeCard from '@/components/home/HomeCard.vue'
 import IconButton from '@/components/common/IconButton.vue'
 
-import { dateToInt, intToDate } from '@/api/TransactionApi'
-import { useTransactionStore } from '@/api/transaction-store'
-
-import { useTransactionStoreAdapter } from '@/stores/transactionStoreAdapter'
 
 // dayjs 추가
 import dayjs from 'dayjs'
@@ -97,9 +93,9 @@ const isFilterOpen = ref(false)
 // 컴포넌트 마운트 시 데이터 로드
 onMounted(async () => {
   // 시작 시 모든 트랜잭션을 가져온 다음 필터링
-  await transactionStore.fetchAllTransactions()
+  // await transactionStore.fetchAllTransactions()
   // 현재 월에 해당하는 데이터만 필터링
-  await loadTransactionsForCurrentMonth()
+  // await loadTransactionsForCurrentMonth()
 })
 
 // 선택한 날짜가 변경될 때 데이터 다시 로드
@@ -120,16 +116,17 @@ watch(selectedDate, async () => {
 // })
 
 import { fetchTransactionsByMonth } from '@/api/TransactionApi.js'
-import axios from "axios";
+import axios from 'axios'
+import * as transactionStore from '@/api/TransactionApi.js'
 
 const getIncomeMonth = async (year, month) => {
   const response = await fetchTransactionsByMonth(year, month)
   const income = ref(0)
   for (const responseElement of response) {
-    if (responseElement.type === '수입') {
+    if (responseElement.type === 'INCOME') {
       income.value += responseElement.amount
     }
-    return income.value;
+    return income.value
   }
 }
 
@@ -137,24 +134,23 @@ const getExponseMonth = async (year, month) => {
   const response = await fetchTransactionsByMonth(year, month)
   const outcome = ref(0)
   for (const responseElement of response) {
-    if (responseElement.type === '수입') {
+    if (responseElement.type === 'EXPENDITURE') {
       outcome.value += responseElement.amount
     }
-    return outcome.value;
+    return outcome.value
   }
 }
 
-
-// const props = defineProps({
-//   income: {
-//     type: Number,
-//     default: 0,
-//   },
-//   expenditure: {
-//     type: Number,
-//     default: 0,
-//   },
-// })
+const props = defineProps({
+  income: {
+    type: Number,
+    default: 0,
+  },
+  expenditure: {
+    type: Number,
+    default: 0,
+  },
+})
 
 // emit으로 거래 클릭 이벤트 전달
 
@@ -178,9 +174,34 @@ const isCurrentMonth = computed(() => {
 const goToPrevMonth = () => {
   selectedMonth.value = dayjs(selectedMonth.value).subtract(1, 'month').format('YYYY-MM')
   // api 에서 월 넣어서 transaction 받아서
-  // 수입만 받아서 income 상태에 넣으세요
-  // 인컴에만 넣으면 홈카드는 자동으로 업데이트
+  console.log('이전 달 이동:', selectedMonth.value)
+  let split = selectedMonth.value.split('-')
 
+  const months = transactionStore.fetchTransactionsByMonth(split[0], split[1])
+  // 수입만 받아서 income 상태에 넣으세요
+  let totalAmount = 0
+  let expense = 0
+  months
+    .then((items) => {
+      items.forEach((item) => {
+        if (item.type === 'INCOME') {
+          totalAmount += item.amount
+        } else {
+          expense += item.amount
+        }
+      })
+      console.log('총 금액:', totalAmount)
+      // props.income = totalAmount
+      // props.expenditure = expense
+    })
+    .catch((error) => {
+      console.error('에러 발생:', error)
+    })
+
+  // console.log('수입 :', props.income)
+  // console.log('지출 :', props.expenditure)
+
+  // 인컴에만 넣으면 홈카드는 자동으로 업데이트
 }
 
 // 다음 달 이동
@@ -191,7 +212,6 @@ const goToNextMonth = () => {
   // api 에서 월 넣어서 transaction 받아서
   // 지출만 받아서 (요청할때 쿼리 때려도 되고. 받아와서 필터링해도 됨) expense 상태에 넣으세요
   // expense에만 넣으면 홈카드는 자동으로 업데이트
-
 }
 
 // 달 클릭 시 date-picker 열기
@@ -206,7 +226,7 @@ const handleMonthChange = (val) => {
 
 // 순수익 = 수입 - 지출
 const NetProfit = computed(() => {
-  const result = 10000 - 1000
+  const result = props.income - props.expenditure
   return isNaN(result) ? 0 : result
 })
 </script>
